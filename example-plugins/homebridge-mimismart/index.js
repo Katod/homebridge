2e8863fdfe4890c0146f9e7b2cd0af5dd6082d3a
Katod <ivanbozhenko@gmail.com>
2016-11-15 15:32:47 +0200

    v 0.0.1 add id subid, lamp change state

--- example-plugins/homebridge-mimismart/index.js
+++ example-plugins/homebridge-mimismart/index.js
@@ -1,4 +1,31 @@
 var Service, Characteristic;
+var socket = require('net').Socket();
+var struct = require('c-struct');
+
+var PORT = 55555;
+var HOST = '192.168.1.124';
+
+var dgram = require('dgram');
+
+var Request = new struct.Schema({
+  SenderID: struct.type.uint16,
+  DestID: struct.type.uint16,
+  PD: struct.type.uint8,
+  Transaction: struct.type.uint8,
+  SenderSubID: struct.type.uint8,
+  DestSubID: struct.type.uint8,
+  Length: struct.type.uint16,
+  Data: struct.type.uint8
+});
+
+// register to cache
+struct.register('rq', Request);
+
+// object to buffer | this can be on another file
+
+var client = dgram.createSocket('udp4');
+
+client.bind(55555);
 
 module.exports = function( homebridge ) {
   Service = homebridge.hap.Service;
@@ -12,13 +39,12 @@ function LampAccessoryMimi( log, config ) {
   this.name = config.name;
   this.id = config.id;
   this.subid = config.subid;
-  
   this.power = 0;
-  this.log( "Initialized '" + this.name + "'" + this.id + "'"+ this.subid + "'");
+  this.log( "Initialized '" + typeof this.name + "'" + this.id + "'"+ this.subid + "'");
 }
 
 LampAccessoryMimi.prototype.getServices = function() {
-  var lightbulbService = new Service.Lightbulb( this.name );
+  var lightbulbService = new Service.Lightbulb(this.name + this.id + this.subid);
   var bulb = this;
 
   lightbulbService
@@ -28,6 +54,23 @@ LampAccessoryMimi.prototype.getServices = function() {
     } )
     .on( 'set', function( value, callback ) {
       bulb.power = value;
+      
+var buf = struct.packSync('rq', {
+         SenderID:2022,
+         DestID:bulb.id,
+         PD:5,
+         Transaction:0,
+         SenderSubID:0,
+         DestSubID:bulb.subid,
+         Length:1,
+         Data:value,
+         });
+
+  client.send(buf, 0, buf.length, PORT, HOST, function(err, bytes) {
+       if (err) throw err;
+      //bulb.log( buf );
+      //bulb.log( buf2 );  
+   });
       bulb.log( "power to " + value );
       callback();
     } );
